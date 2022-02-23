b5ef392e75f017455b4acde5212a72473a957522
wujilei <wujilei5@126.com>
2022-02-23 18:05:30 +0800

    filter

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ new Vue({
         isMorning: hours <= 12,
         isAfternoon: hours > 12,
         inputType: "checkbox",
+        product1Price: 1234,
+        product2Price: 2345,
+        product3Price: 32456,
+    },
+    filters: {
+      formatPrice(value) {
+        return (value/100).toFixed(2);
+      }  
     },
     created(){
         console.log("hahahah");
