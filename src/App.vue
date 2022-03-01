5c96ef100924a4b9838fef752e9d991cfc11137d
wujilei <wujilei5@126.com>
2022-03-01 18:32:47 +0800

    完善曲线图

--- src/App.vue
+++ src/App.vue
@@ -26,20 +26,60 @@ export default {
             title: {
                 text: '历年温度曲线'
             },
-            tooltip: {},
+            legend: {
+                show: true,
+            },
+            tooltip: {
+                 trigger: 'axis',
+                position: function (pt) {
+                    return [pt[0], '10%'];
+                }
+            },
+            dataZoom: [
+                {
+                    type: 'inside',
+                    start: 0,
+                    end: 100
+                },
+                // {
+                //     start: 0,
+                //     end: 20
+                // }
+            ],
             xAxis: {
-                type: 'time',
+                type: 'category',
                 boundaryGap: false,
-                
+                data:[],
             },
             yAxis: {
                 type: 'value',
-                min: 0, 
+                min: -10, 
                 max: 30,
             },
             series: [
                 {
                     name: '2018供暖季',
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    data: [],
+                },
+                {
+                    name: '2019供暖季',
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    data: [],
+                },
+                {
+                    name: '2020供暖季',
+                    type: 'line',
+                    smooth: true,
+                    symbol: 'none',
+                    data: [],
+                },
+                {
+                    name: '2021供暖季',
                     type: 'line',
                     smooth: true,
                     symbol: 'none',
@@ -63,18 +103,37 @@ export default {
                 res.data.V1.forEach((v)=>{
                     // console.log(v.Dt);
                     let averTemp = (v.Temp.Max + v.Temp.Min)/2;
-                    option.series[0].data.push([new Date(v.Dt*1000),averTemp]);
+                    option.xAxis[0].data.push(this.formatDate(v.Dt));
+                    option.series[0].data.push(averTemp);
+                });
+                res.data.V2.forEach((v)=>{
+                    let averTemp = (v.Temp.Max + v.Temp.Min)/2;
+                    // option.xAxis[0].data.push(this.formatDate(v.Dt));
+                    option.series[1].data.push(averTemp);
+                });
+                res.data.V3.forEach((v)=>{
+                    let averTemp = (v.Temp.Max + v.Temp.Min)/2;
+                    // option.xAxis[0].data.push(this.formatDate(v.Dt));
+                    option.series[2].data.push(averTemp);
+                });
+                res.data.V4.forEach((v)=>{
+                    let averTemp = (v.Temp.Max + v.Temp.Min)/2;
+                    // option.xAxis[0].data.push(this.formatDate(v.Dt));
+                    option.series[3].data.push(averTemp);
                 });
                 this.mychart.setOption(option);
             });
             
+        },
+        formatDate:function(dt) {
+            let date = new Date(dt*1000);
+            return date.getMonth()+1 + "-" + date.getDate();
         },
     }
 }
 </script>
 <style>
 .chart {
-    width: 900px;
     height: 500px;
 }
 </style>
